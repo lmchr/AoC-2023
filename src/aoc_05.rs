6def0dc707284361b8065fe57a24abdbc6f6fdf4
larsm <lars.melcher94@gmail.com>
2023-12-06 19:21:02 +0100

    parallel (working) very slow day 5 part 2

--- src/aoc_05.rs
+++ src/aoc_05.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 use core::ops::Range;
+use std::cmp;
+use rayon::prelude::*;
 
 pub fn main(inputs: &[String]) {
     println!("Part 1: {}", part1(inputs));
     println!("Part 2: {}", part2(inputs));
 }
 
-fn part_1(inputs: &[String], seeds: &Vec<i64>) -> i64 {
+fn part_1(inputs: &[String], is_p2: bool) -> i64 {
     let mut collect = vec![];
     let mut collect_tmp = vec![];
     for x in inputs
@@ -49,6 +51,44 @@ fn part_1(inputs: &[String], seeds: &Vec<i64>) -> i64 {
         x_to_y_map.clear();
     }
 
+    let mut min = i64::MAX;
+    if is_p2 {
+        // brute force, take <5min
+        for x in inputs
+            .first()
+            .unwrap()
+            .strip_prefix("seeds: ")
+            .unwrap()
+            .split(' ')
+            .map(|s| s.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>()
+            .chunks(2){
+            let sub_results = (x[0]..(x[0] + x[1]))
+                .into_par_iter()
+                .map(|seed| get_location_by_seed(&main_collect, &seed))
+                .min()
+                .unwrap();
+            min = cmp::min(min, sub_results)
+        }
+    } else {
+        let first_line = inputs.first().unwrap();
+        let result = first_line
+            .strip_prefix("seeds: ")
+            .unwrap()
+            .split(' ')
+            .map(|s| s.parse::<i64>().unwrap())
+            .collect::<Vec<_>>()
+            .into_par_iter()
+            .map(|seed| get_location_by_seed(&main_collect, &seed))
+            .min()
+            .unwrap();
+        min = result;
+    }
+    min
+}
+
+fn get_location_by_seed(main_collect: &HashMap<(String, String), Vec<(Range<i64>, Range<i64>)>>, seed: &i64) -> i64 {
+
     fn get_next_value(main_collect: &HashMap<(String, String), Vec<(Range<i64>, Range<i64>)>>,
                       from: &str, to: &str,
                       val: &i64) -> i64 {
@@ -61,43 +101,20 @@ fn part_1(inputs: &[String], seeds: &Vec<i64>) -> i64 {
         *val  // Any source numbers that aren't mapped correspond to the same destination number.
     }
 
-    // map the initial seeds to the final destination
-    let mut final_locations = Vec::with_capacity(seeds.len());
-    for seed in seeds {
-        let soil: i64 = get_next_value(&main_collect, "seed", "soil", seed);
-        let fertilizer: i64 = get_next_value(&main_collect, "soil", "fertilizer", &soil);
-        let water: i64 = get_next_value(&main_collect, "fertilizer", "water", &fertilizer);
-        let light: i64 = get_next_value(&main_collect, "water", "light", &water);
-        let temperature: i64 = get_next_value(&main_collect, "light", "temperature", &light);
-        let humidity: i64 = get_next_value(&main_collect, "temperature", "humidity", &temperature);
-        let location: i64 = get_next_value(&main_collect, "humidity", "location", &humidity);
-        final_locations.push(location);
-    }
-    *final_locations.iter().min().unwrap()
+    let soil: i64 = get_next_value(&main_collect, "seed", "soil", &seed);
+    let fertilizer: i64 = get_next_value(&main_collect, "soil", "fertilizer", &soil);
+    let water: i64 = get_next_value(&main_collect, "fertilizer", "water", &fertilizer);
+    let light: i64 = get_next_value(&main_collect, "water", "light", &water);
+    let temperature: i64 = get_next_value(&main_collect, "light", "temperature", &light);
+    let humidity: i64 = get_next_value(&main_collect, "temperature", "humidity", &temperature);
+    let location: i64 = get_next_value(&main_collect, "humidity", "location", &humidity);
+    location
 }
 
 pub fn part1(inputs: &[String]) -> i64 {
-    let first_line = inputs.first().unwrap();
-    let seeds: Vec<i64> = first_line
-        .strip_prefix("seeds: ")
-        .unwrap()
-        .split(' ')
-        .map(|s| s.parse::<i64>().unwrap())
-        .collect();
-
-    part_1(&inputs, &seeds)
+    part_1(&inputs, false)
 }
 
 pub fn part2(inputs: &[String]) -> i64 {
-    let first_line = inputs.first().unwrap();
-    let seeds: Vec<i64> = first_line
-        .strip_prefix("seeds: ")
-        .unwrap()
-        .split(' ')
-        .map(|s| s.parse::<i64>().unwrap())
-        .collect::<Vec<i64>>()
-        .chunks(2)
-        .flat_map(|x| (x[0].. x[0] + x[1]).collect::<Vec<i64>>())
-        .collect();
-    part_1(inputs, &seeds)
+    part_1(inputs, true)
 }
--- tests/test_aoc.rs
+++ tests/test_aoc.rs
@@ -41,7 +41,7 @@ mod tests {
     fn test_aoc_05() {
         let inputs = util::read_input(&Days::FIVE);
         assert_eq!(aoc_2023::aoc_05::part1(&inputs), 484023871);
-        // assert_eq!(aoc_2023::aoc_05::part2(&inputs), 5667240);
+        assert_eq!(aoc_2023::aoc_05::part2(&inputs), 46294175);
     }
 
 }
