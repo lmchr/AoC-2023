70a853c1bb643b8baa4d9fe0704305964aa3b520
lmchr <lars.melcher94@gmail.com>
2023-12-16 18:31:45 +0100

    lint

--- src/aoc_10.rs
+++ src/aoc_10.rs
@@ -16,8 +16,7 @@ fn parse_input(inputs: &[String]) -> Vec<Vec<char>>{
         .collect::<Vec<Vec<char>>>()
 }
 
-fn get_char_at_pos<T: std::fmt::Debug + Clone + Copy>(inputs: &Vec<Vec<T>>, pos: &(i32, i32)) -> Option<T> {
-    // println!("checking {:?}", pos);
+fn get_char_at_pos<T: std::fmt::Debug + Clone + Copy>(inputs: &[Vec<T>], pos: &(i32, i32)) -> Option<T> {
     if let Some(x) = inputs.get(pos.0 as usize) {
         if let Some(&y) = x.get(pos.1 as usize) {
             return Some(y);
@@ -78,13 +77,13 @@ impl Symbols {
 
 fn recurse(inputs: &Vec<Vec<char>>, mut distances: Vec<Vec<i32>>, distance_from_s: i32, current_pos: &(i32, i32)) -> Vec<Vec<i32>> {
     //println!("recursion at {:?}", current_pos);
-    if let Some(current_pos_char) = get_char_at_pos(inputs, &current_pos) {
+    if let Some(current_pos_char) = get_char_at_pos(inputs, current_pos) {
         //println!("Recurse, level: {distance_from_s}, currently at {current_pos:?}, char: {current_pos_char}");
         let lookup = Symbols::by_value(&current_pos_char);
         if lookup != Symbols::Dot {
             // println!("Setting for char {}={}; current symbol: {lookup:?}", inputs[current_pos.0 as usize][current_pos.1 as usize], distance_from_s);
             distances[current_pos.0 as usize][current_pos.1 as usize] = distance_from_s;
-            let mut current_pos_new = current_pos.clone();
+            let mut current_pos_new = *current_pos;
             if lookup != Symbols::S {
                 let distance_lookup = get_char_at_pos(&distances, &(current_pos_new.0 + lookup.value().0.0, current_pos_new.1 + lookup.value().0.1));
                 let input_lookup = get_char_at_pos(inputs, &(current_pos_new.0 + lookup.value().0.0, current_pos_new.1 + lookup.value().0.1));
@@ -118,10 +117,10 @@ fn recurse(inputs: &Vec<Vec<char>>, mut distances: Vec<Vec<i32>>, distance_from_
 }
 
 fn figure_out_s_symbol(inputs: &&Vec<Vec<char>>, current_pos: &&(i32, i32), current_pos_new: &mut (i32, i32)) -> Symbols {
-    let north = get_char_at_pos(&inputs, &(current_pos_new.0 - 1, current_pos.1));
-    let east = get_char_at_pos(&inputs, &(current_pos_new.0, current_pos.1 + 1));
-    let south = get_char_at_pos(&inputs, &(current_pos_new.0 + 1, current_pos.1));
-    let west = get_char_at_pos(&inputs, &(current_pos_new.0, current_pos.1 - 1));
+    let north = get_char_at_pos(inputs, &(current_pos_new.0 - 1, current_pos.1));
+    let east = get_char_at_pos(inputs, &(current_pos_new.0, current_pos.1 + 1));
+    let south = get_char_at_pos(inputs, &(current_pos_new.0 + 1, current_pos.1));
+    let west = get_char_at_pos(inputs, &(current_pos_new.0, current_pos.1 - 1));
     let is_north = if let Some(north_) = north {[Symbols::PipeVertical.as_char(), Symbols::F.as_char(), Symbols::Seven.as_char()].contains(&north_)} else { false };
     let is_east = if let Some(east_) = east {[Symbols::PipeHorizontal.as_char(), Symbols::J.as_char(), Symbols::Seven.as_char()].contains(&east_)} else { false };
     let is_south = if let Some(south_) = south {[Symbols::PipeVertical.as_char(), Symbols::J.as_char(), Symbols::L.as_char()].contains(&south_)} else { false };
